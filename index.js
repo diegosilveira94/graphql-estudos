ad1c100a7711e1d41cf3ac63aa1e1b843f29fac0
Diego Silveira <diegocilveira@gmail.com>
2025-11-08 00:46:17 -0300

    aula12 refact folders, resolvers and types separated

--- index.js
+++ index.js
@@ -1,71 +1,8 @@
-const { gql, ApolloServer } = require('apollo-server');
-
-const produtos = [
-    {
-        id: '1',
-        nome: 'Notebook',
-        preco: 2500.00
-    },
-    {
-        id: '2',
-        nome: 'Smartphone',
-        preco: 1500.00
-    }
-];
-
-const usuarios = [
-    {
-        id: '1',
-        nome: 'João Silva',
-        salario: 3500.75,
-        idade: 28,
-        ativo: true
-    },
-    {
-        id: '2',
-        nome: 'Maria Oliveira',
-        salario: 4200.00,
-        idade: 34,
-        ativo: false
-    }
-];
-
-const typeDefs = gql`
-	
-    type Produto {
-        id: ID
-        nome: String
-        preco: Float
-    }
-
-	type Usuario {
-		idade: Int
-		salario: Float
-		nome: String
-		ativo: Boolean
-		id: ID
-	}
-	
-	type Query {
-		usuarios: [Usuario]
-        produtos: [Produto]
-	}
-`;
-
-const resolvers = {
-    Query: {
-        usuarios: () => {
-            return usuarios;
-        },
-        produtos: () => {
-            return produtos;
-        }
-    }
-};
-
+const { ApolloServer } = require("apollo-server");
+const graphql = require("./src/graphql");
+// Refatoração do código com estruturação das pastas separadas dos types e resolvers
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  ...graphql,
 });
 
-server.listen();
+server.listen().then(({ url }) => console.log(url));
---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  usuarios: [
+    {
+      id: 1,
+      nome: "Rafael",
+      email: "rafael@gmail.com",
+      telefone: "(11) 99999-9999",
+      perfil: 1,
+    },
+    {
+      id: 2,
+      nome: "Maria",
+      email: "maria@gmail.com",
+      telefone: "(21) 98888-8888",
+      perfil: 2,
+    },
+  ],
+  perfis: [
+    { id: 1, descricao: "ADMIN" },
+    { id: 2, descricao: "NORMAL" },
+  ],
+};
