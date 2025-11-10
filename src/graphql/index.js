dc8506fe569df32786627303967183cb2ccd99a0
Diego Silveira <diegosilveira@NG0568.ng.local>
2025-11-10 12:14:25 -0300

    initial commit new branch

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,13 +1,13 @@
-const { join } = require("path");
-const { loadFilesSync } = require("@graphql-tools/load-files");
-const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
+// const { join } = require("path");
+// const { loadFilesSync } = require("@graphql-tools/load-files");
+// const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
 
-const allTypes = loadFilesSync(join(__dirname, "modules", "**", "*.gql"));
-const allResolvers = loadFilesSync(
-  join(__dirname, "modules", "**", "resolvers.js")
-);
+// const allTypes = loadFilesSync(join(__dirname, "modules", "**", "*.gql"));
+// const allResolvers = loadFilesSync(
+//   join(__dirname, "modules", "**", "resolvers.js")
+// );
 
-const typeDefs = mergeTypeDefs(allTypes);
-const resolvers = mergeResolvers(allResolvers);
+// const typeDefs = mergeTypeDefs(allTypes);
+// const resolvers = mergeResolvers(allResolvers);
 
-module.exports = { typeDefs, resolvers };
+// module.exports = { typeDefs, resolvers };
