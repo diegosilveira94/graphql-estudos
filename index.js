a8ac055ffe8467172981eb478cfa587a4419a546
Diego Silveira <diego.silveira@ngi.com.br>
2025-11-14 13:23:13 -0300

    aula20 - adding api git-tasks

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { ApolloServer } = require("apollo-server");
 const graphql = require("./src/graphql");
 const GitHubService = require("./src/services/GitHub.services");
+const UserRegisterService = require("./src/services/UserRegisterService");
+const TasksRegisterService = require("./src/services/TasksRegisterService");
 
 const server = new ApolloServer({
   ...graphql,
   dataSources: () => ({
     gitHubService: GitHubService,
+    userRegisterService: UserRegisterService,
+    tasksService: TasksRegisterService,
   }),
 });
 
--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,7 +1,23 @@
 module.exports = {
+  User: {
+    async tasks(user, _, { dataSources }) {
+      return await dataSources.tasksService.getTasks(user.id);
+    },
+  },
   Query: {
     async user(_, { login }, { dataSources }) {
-      return await dataSources.gitHubService.getUser(login);
+      const userFound =
+        await dataSources.userRegisterService.getUserByLogin(login);
+
+      if (userFound) return userFound;
+
+      const { login: loginGit, avatar_url } =
+        await dataSources.gitHubService.getUser(login);
+
+      return await dataSources.userRegisterService.addUser({
+        login: loginGit,
+        avatar_url,
+      });
     },
   },
 };
