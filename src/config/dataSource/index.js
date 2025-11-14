b0169dd373e613e54bb2a7a29e6f809487cb2b1d
Diego Silveira <diego.silveira@ngi.com.br>
2025-11-14 18:18:26 -0300

    aula25 - using typeScript with sucrase

--- src/config/dataSource/index.js
+++
@@ -1,9 +0,0 @@
-const GitHubServices = require("../../services/GitHub.services");
-const TasksRegisterService = require("../../services/TasksRegisterService");
-const UserRegisterService = require("../../services/UserRegisterService");
-
-module.exports = () => ({
-  gitHubService: GitHubServices,
-  userRegisterService: UserRegisterService,
-  tasksService: TasksRegisterService,
-});
--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-const dataSources = require("./dataSource");
-const context = require("./context");
-const formatError = require("./formatError");
-
-module.exports = {
-  dataSources,
-  context,
-  formatError,
-};
